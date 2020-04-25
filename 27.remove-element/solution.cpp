200f1f7b710c658380314ff645294b44339dac5c
Devin xu <977501187@qq.com>
2020-04-25 13:41:09 +0800

    add remove-element

---
+++ 27.remove-element/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int ret = 0;
+        for (int i = 0; i<nums.size(); i++) {
+            if (nums[i] != val)
+                nums[ret++] = nums[i];
+        }
+        return ret;
+    }
+};
